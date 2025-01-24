d2e066dc11a04de91e345a176fee2d623d845d3b
Joowon-Seo <90291431+Joowon-Seo@users.noreply.github.com>
2025-01-24 18:00:24 +0900

    refactor : index.js 수정

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 async function makeIssue() {
-    const token = process.env.GITHUB_TOKEN;
-    const OWNER = "Joowon-Seo" // github 계정 이름
-    const REPO = "github_power_action" // 현재 repository 이름
+    const token = process.env.GH_TOKEN; // 주의! 일치해야함 (env와 일치해야함)
+    // 요고 1
+    const OWNER = "Joowon-Seo"; // 여러분들의 github 계정 이름
+    // 요고 2
+    const REPO = "github_power_action"; // 여러분들의 현재 리포지터리 이름
     const response = await fetch(`https://api.github.com/repos/${OWNER}/${REPO}/issues`, {
         method: 'POST',
         headers: {
             Authorization: `Bearer ${token}`,
         },
         body: JSON.stringify({
-            title: "행운의 숫자", // 수정 가능 부분
-            body: `$(Math.floor(Math.random() * 100) + 1)`, // 수정 가능 부분
+            title: "행운의 숫자", // 요고 3
+            body: `${Math.floor(Math.random() * 100) + 1}`, // 요고 4
         })
     });
-    if(response.ok) {
+    if (response.ok) {
         console.log("성공");
     } else {
         console.log("실패");
